8bee5978a67f71ac626ae40a80b6eb4a965076c0
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-29 17:37:17 +0530

    Add: create hello react section in UI

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='introcontainer'>
+        <h2 className='introcontainer__title'>Hello React....</h2>
+        <p>Pranshu Patel</p>
+        <p>Hello, I am a react trainee.</p>
+        <p>Email: <a href="mailto: pranshu.patel@simformsolutions.com">pranshu.patel@simformsolutions.com</a></p>
+        <p>Github: <a href="https://github.com/Pranshu1852">Pranshu1852</a></p>
+      </div>
+    </>
+  )
+}
+
+export default App
